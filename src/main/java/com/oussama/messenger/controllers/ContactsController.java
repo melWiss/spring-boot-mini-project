40ffdcc1254e11236f553a254fd8ba61af2bbeba
Oussama Maatallah <oussamamaatallah01@gmail.com>
2022-03-11 18:05:57 +0100

    finished Contact CRUD controller.

--- src/main/java/com/oussama/messenger/controllers/ContactsController.java
+++ src/main/java/com/oussama/messenger/controllers/ContactsController.java
@@ -1,13 +1,13 @@
 package com.oussama.messenger.controllers;
 
 import com.oussama.messenger.entities.Contact;
+import com.oussama.messenger.entities.ContactType;
 import com.oussama.messenger.repositories.ContactRepo;
 import com.oussama.messenger.services.ContactService;
 import com.oussama.messenger.services.ContactServiceImpl;
+import com.oussama.messenger.services.ContactTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,25 @@ import java.util.List;
 public class ContactsController {
     @Autowired
     ContactService contactService;
+
     @GetMapping(value = "/all")
-    String hi(){
+    String fetchAll(){
         return contactService.getAllContacts().toString();
     }
+
+    @PostMapping(value = "/add")
+    String addContact(@RequestBody Contact type){
+        Contact t = contactService.persistContact(type);
+        return t.toString();
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    String deleteContact(@PathVariable Long id){
+        return contactService.deleteContact(id).toString();
+    }
+
+    /*@PutMapping(value = "/update")
+    String updateContactType(@RequestBody ContactType type){
+        return contactTypeService.updateContactType(type).toString();
+    }*/
 }
